8b96dd3d101637887772971dbddb9ab3ce2acfce
alexj <a.jordan6955@gmail.com>
2021-11-18 12:35:37 -0700

    Setter method added for updating states

--- scripts/structs/base_states.py
+++ scripts/structs/base_states.py
@@ -2,15 +2,22 @@ import numpy as np
 
 class BaseStates:
    def __init__(self,p0,euler0,vb0):
-      self.p = p0  
-      self.euler = euler0
-      self.vb = vb0
+      self.p = p0                   # Boat position
+      self.euler = euler0           # Attitude in euler
+      self.vb = vb0                 # boat velocity in boat frame
+      self.bias = np.zeros((3,1))   # Gyro bias
 
       # Low pass filtered gyro measurments
       self.wLpf = np.zeros((3,1))
-
       self.alpha = 0.1
 
    def update_w_lpf(self,gyros):
       # Pass the gyros through a low-pass filter
       self.wLpf = self.alpha*gyros+(1-self.alpha)*self.wLpf
+
+   def update_state(self, belief, phi, theta, bias):
+      """Update state estimate from estimator class"""
+      self.p = belief.p
+      self.euler = np.array([[phi.squeeze(),theta.squeeze(),belief.psi.squeeze()]]).T
+      self.vb = belief.vb
+      self.bias = bias
