70cb539f782b50859ea9940a4c18384cd6c370be
alexj <a.jordan6955@gmail.com>
2021-11-18 12:37:35 -0700

    Test for complementary filter added. Unused tests deleted. Now using pytest.

--- tests/test_bias.py
+++ tests/test_bias.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('/home/matt/px4_ws/src/boat_estimator/scripts/estimator')
-sys.path.append('/home/matt/px4_ws/src/boat_estimator/scripts/structs')
+sys.path.append('/home/alex/px4_ws/src/boat_estimator/scripts/estimator')
+sys.path.append('/home/alex/px4_ws/src/boat_estimator/scripts/structs')
 import numpy as np
 
 import unittest
@@ -9,32 +9,30 @@ from estimator_class import Estimator
 from sensors import ImuMsg
 from sensors import GpsMsg
 
-class TestBias(unittest.TestCase):
-    def test_pitch_xz_acceleration(self):
-        time = 123.023
-        accelerometers = [0.0,0.0,-9.81]
-        gyros = [0.0,1.0,0.0]
-        imu = ImuMsg(time,accelerometers,gyros)
-        dt = 0.1
 
-        estimator = Estimator()
-        estimator.imuPrevTime = time
-        steps = 5
-        for i in range(steps):
-            imu.time = estimator.imuPrevTime + dt
-            pPrev = estimator.belief.p
-            qPrev = estimator.belief.q
-            vPrev = estimator.belief.v
-            baPrev = estimator.belief.ba
-            bgPrev = estimator.belief.bg
-            estimator.imu_callback(imu)
-        positionEcef = [-1800198.22956286, -4532912.78883003,  4098399.82768237]
-        velocityEcef = [0.0,0.0,0.0]
-        gps = GpsMsg(positionEcef,velocityEcef)
-        estimator.set_ref_lla_callback(40.23,-111.66,1370.0)
-        estimator.gps_callback(gps)
-        print('accelerometer bias = ', estimator.belief.ba)
-        print('gyro bias = ', estimator.belief.bg)
+def test_pitch_xz_acceleration(self):
+    time = 123.023
+    accelerometers = [0.0,0.0,-9.81]
+    gyros = [0.0,1.0,0.0]
+    imu = ImuMsg(time,accelerometers,gyros)
+    dt = 0.1
 
-if __name__ == '__main__':
-    unittest.main()
+    estimator = Estimator()
+    estimator.imuPrevTime = time
+    steps = 5
+    for i in range(steps):
+        imu.time = estimator.imuPrevTime + dt
+        pPrev = estimator.belief.p
+        qPrev = estimator.belief.q
+        vPrev = estimator.belief.v
+        baPrev = estimator.belief.ba
+        bgPrev = estimator.belief.bg
+        estimator.imu_callback(imu)
+    positionEcef = [-1800198.22956286, -4532912.78883003,  4098399.82768237]
+    velocityEcef = [0.0,0.0,0.0]
+    gps = GpsMsg(positionEcef,velocityEcef)
+    estimator.set_ref_lla_callback(40.23,-111.66,1370.0)
+    estimator.gps_callback(gps)
+    print('accelerometer bias = ', estimator.belief.ba)
+    print('gyro bias = ', estimator.belief.bg)
+
--- tests/test_comp_filt.py
+++ tests/test_comp_filt.py
@@ -1,59 +1,67 @@
 import sys
-sys.path.append('/home/matt/px4_ws/src/boat_estimator/scripts/estimator')
-sys.path.append('/home/matt/px4_ws/src/boat_estimator/scripts/structs')
+import os
+print(os.getcwd())
+sys.path.append('./scripts/estimator')
+sys.path.append('./scripts/structs')
+#sys.path.append('..')
+print(os.getcwd())
 import numpy as np
 
-import unittest
-import comp_filter
-from states_covariance import StatesCovariance
+import pytest
+from base_states import BaseStates
+from comp_filter import compFilter
 from sensors import ImuMsg
 
-class TestComplimentaryFilter(unittest.TestCase):
-    # def test_zero_inputs(self):
-    #     p0 = np.zeros((3,1))
-    #     q0 = np.zeros((3,1))
-    #     v0 = np.zeros((3,1))
-    #     ba0 = np.zeros((3,1))
-    #     bg0 = np.zeros((3,1))
-    #     cov0 = np.array([[1.0,1.0,1.0]]).T
-    #     belief = StatesCovariance(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
-    #     accelerometers = [0.0,0.0,-9.81]
-    #     gyros = [0.0,0.0,0.0]
-    #     imu = ImuMsg(0.0,accelerometers,gyros)
-    #     dt = 0.1
-    #     kp = 0.1
-    #     ki = 0.9
-    #     comp_filter.run(belief,imu,dt,kp,ki)
-    #
-    #     qExpected = np.array([[0.0,0.0,0.0]]).T
-    #
-    #     self.assert_attitude(belief.q,qExpected)
+# Orientaitons for tests of complementary filter on a stationary boat in degrees.
+@pytest.mark.parametrize("euler", [(0,0,0), (0,10,0), (10,0,0), (10,-10,0), (-10,-10,0)])
 
-    def test_same_inputs(self):
-        p0 = np.zeros((3,1))
-        q0 = np.array(([[0.0,0.5036,0.0]])).T
-        v0 = np.zeros((3,1))
-        ba0 = np.zeros((3,1))
-        bg0 = np.zeros((3,1))
-        cov0 = np.array([[1.0,1.0,1.0]]).T
-        belief = StatesCovariance(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
-        accelerometers = [-4.905,0.0,-8.496]
-        gyros = [0.0,0.2,0.0]
-        imu = ImuMsg(0.0,accelerometers,gyros)
-        dt = 0.1
-        kp = 0.1
-        ki = 0.9
-        comp_filter.run(belief,imu,dt,kp,ki)
+def test_no_change(euler):
+    # Test that comp filter roughly matches the accelerometer when the base is stationary at a given orientation
+    p0 = np.zeros((3,1))
+    q0 = np.radians(euler)
+    v0 = np.zeros((3,1))
+    base = BaseStates(p0, q0, v0)
 
-        qExpected = np.array([[0.0,0.0,0.0]]).T
+    # Filter object
+    kp = 0.1
+    ki = 0.9
+    cf = compFilter(kp, ki)
 
-        self.assert_attitude(belief.q,qExpected)
+    # Generate IMUs
+    gyro_noise = np.random.normal(size=(3,1))*.00001
+    accel_noise = np.random.normal(size=(3,))*.00001
+    accelerometers = [9.81*np.sin(q0.item(1))+accel_noise.item(0),
+                     -9.81*np.cos(q0.item(1))*np.sin(q0.item(0))+accel_noise.item(1),
+                     -9.81*np.cos(q0.item(1))*np.cos(q0.item(0))+accel_noise.item(2)]
 
-    def assert_attitude(self,q,qExpected):
-        decimalPlaces = 3
-        for i in range(3):
-            self.assertAlmostEqual(q.item(i),qExpected.item(i),decimalPlaces)
+    
+    gyros = [gyro_noise.item(0), gyro_noise.item(1), gyro_noise.item(2)]
+    imu = ImuMsg(0.0,accelerometers,gyros)
+    dt = 0.1
+    phi, theta, bias = cf.run(base,imu,dt)
+
+    qExpected = q0
+
+    # Phi
+    tol = 3e-3
+    assert np.isclose(phi, qExpected.item(0), atol=tol)
+
+    # Theta
+    assert np.isclose(theta, qExpected.item(1), atol=tol)
+    """
+    # Pitched up at 20 deg, no gyro movement
+    pitch = np.radians(12.55)
+    q0 = np.array(([[0.0,pitch,0.0]])).T
+    accelerometers = [9.81*np.sin(pitch),0.0,-9.81*np.cos(pitch)]
+    imu = ImuMsg(0.0,accelerometers,gyros)
+    
+    base = BaseStates(p0, q0, v0)
+    qExpected = q0
+    u = comp_filter.run(base,imu,dt,kp,ki, bias0)
+    phi, theta = u[6:8]
+
+    # Theta
+    assert np.isclose(theta, qExpected.item(1))"""
 
 
-if __name__ == '__main__':
-    unittest.main()
+
